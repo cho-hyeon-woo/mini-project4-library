5d2055407b681d03a72e27e2efc47c2725fefc88
HyeonWoo Cho <robert12154937@gmail.com>
2026-06-09 14:59:56 +0900

    Book/User/CoverGeneration Entity 필드 추가

--- bookapp/src/main/java/com/aivle/bookapp/domain/CoverGeneration.java
+++ bookapp/src/main/java/com/aivle/bookapp/domain/CoverGeneration.java
@@ -1,7 +1,6 @@
 package com.aivle.bookapp.domain;
 
 import jakarta.persistence.*;
-import jakarta.validation.constraints.NotBlank;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
