bc792cacd9ea97446baa241bccd1ef0464d604c3
HyeonWoo Cho <robert12154937@gmail.com>
2026-06-09 16:29:04 +0900

    repository 골격 추가

--- bookapp/src/main/java/com/aivle/bookapp/repository/CoverGenerationRepository.java
+++ bookapp/src/main/java/com/aivle/bookapp/repository/CoverGenerationRepository.java
@@ -1,10 +1,9 @@
 package com.aivle.bookapp.repository;
 
-
 import com.aivle.bookapp.domain.CoverGeneration;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-// import java.util.List;
+//import java.util.List;
 
 public interface CoverGenerationRepository extends JpaRepository<CoverGeneration, Long> {
 
--- bookapp/src/main/java/com/aivle/bookapp/repository/UserRepository.java
+++ bookapp/src/main/java/com/aivle/bookapp/repository/UserRepository.java
@@ -3,7 +3,7 @@ package com.aivle.bookapp.repository;
 import com.aivle.bookapp.domain.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-// import java.util.List;
+//import java.util.List;
 
 public interface UserRepository extends JpaRepository<User, Long> {
 
