966fe73e5d663a06b735ad77728e841c08fc11bf
Jungyh <wjddudghks048@gmail.com>
2026-06-11 16:52:08 +0900

    feat: 벡인드 북마크 기능 업데이트

---
+++ bookapp/src/main/java/com/aivle/bookapp/repository/BookMarkRepository.java
@@ -0,0 +1,19 @@
+package com.aivle.bookapp.repository;
+
+import com.aivle.bookapp.domain.Book;
+import com.aivle.bookapp.domain.BookMark;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+
+import java.util.Optional;
+import java.util.List;
+
+public interface BookMarkRepository extends JpaRepository<BookMark, Long> {
+    Optional<BookMark> findByUserIdAndBookId(Long userId, Long bookId);
+    boolean existsByUserIdAndBookId(Long userId, Long bookId); 
+
+    @Query("SELECT b FROM Book b WHERE b.id IN (SELECT bm.bookId FROM BookMark bm WHERE bm.userId = :userId)")
+    List<Book> getBookmarkedBooks(Long userId);
+}
